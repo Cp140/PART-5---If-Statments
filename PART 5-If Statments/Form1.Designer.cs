02f671b9c66a077d928b456466a784aef33412d6
Cole Palmer <PalmCole551@GoTVDSB.ca>
2022-03-11 10:45:12 -0500

    Done

--- PART 5-If Statments/Form1.Designer.cs
+++ PART 5-If Statments/Form1.Designer.cs
@@ -30,42 +30,46 @@
         {
             this.btnSubmitAge = new System.Windows.Forms.Button();
             this.txtAge = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblInstructions = new System.Windows.Forms.Label();
             this.lblStage = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnSubmitAge
             // 
-            this.btnSubmitAge.Location = new System.Drawing.Point(433, 168);
+            this.btnSubmitAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmitAge.Location = new System.Drawing.Point(192, 86);
             this.btnSubmitAge.Name = "btnSubmitAge";
-            this.btnSubmitAge.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmitAge.Size = new System.Drawing.Size(91, 37);
             this.btnSubmitAge.TabIndex = 0;
-            this.btnSubmitAge.Text = "button1";
+            this.btnSubmitAge.Text = "Get results";
             this.btnSubmitAge.UseVisualStyleBackColor = true;
             this.btnSubmitAge.Click += new System.EventHandler(this.btnSubmitAge_Click_1);
             // 
             // txtAge
             // 
-            this.txtAge.Location = new System.Drawing.Point(424, 98);
+            this.txtAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAge.Location = new System.Drawing.Point(12, 56);
             this.txtAge.Name = "txtAge";
-            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.Size = new System.Drawing.Size(293, 24);
             this.txtAge.TabIndex = 1;
             // 
-            // label1
+            // lblInstructions
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(199, 117);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(12, 9);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(284, 44);
+            this.lblInstructions.TabIndex = 2;
+            this.lblInstructions.Text = "Enter an age and the program will \r\ntell you what strage if life it is.";
             // 
             // lblStage
             // 
             this.lblStage.AutoSize = true;
-            this.lblStage.Location = new System.Drawing.Point(440, 141);
+            this.lblStage.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStage.Location = new System.Drawing.Point(31, 94);
             this.lblStage.Name = "lblStage";
-            this.lblStage.Size = new System.Drawing.Size(55, 13);
+            this.lblStage.Size = new System.Drawing.Size(91, 22);
             this.lblStage.TabIndex = 3;
             this.lblStage.Text = "Life Stage";
             // 
@@ -73,13 +77,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(332, 141);
             this.Controls.Add(this.lblStage);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblInstructions);
             this.Controls.Add(this.txtAge);
             this.Controls.Add(this.btnSubmitAge);
+            this.ForeColor = System.Drawing.Color.Black;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.White;
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,7 +96,7 @@
 
         private System.Windows.Forms.Button btnSubmitAge;
         private System.Windows.Forms.TextBox txtAge;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblInstructions;
         private System.Windows.Forms.Label lblStage;
     }
 }
